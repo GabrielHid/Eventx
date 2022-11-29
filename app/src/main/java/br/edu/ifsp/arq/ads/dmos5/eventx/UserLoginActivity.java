cb1500598e9a81782daa5128d1fc7e21d5829efc
GcamaraH <g.camara@aluno.ifsp.edu.br>
2022-11-28 23:19:06 -0300

    Implementação de splash screen e backend do login/registro de usuário

--- app/src/main/java/br/edu/ifsp/arq/ads/dmos5/eventx/UserLoginActivity.java
+++ app/src/main/java/br/edu/ifsp/arq/ads/dmos5/eventx/UserLoginActivity.java
@@ -3,12 +3,20 @@ package br.edu.ifsp.arq.ads.dmos5.eventx;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import br.edu.ifsp.arq.ads.dmos5.eventx.model.User;
+import br.edu.ifsp.arq.ads.dmos5.eventx.viewmodel.UserViewModel;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -21,11 +29,54 @@ public class UserLoginActivity extends AppCompatActivity {
     private TextView txtForgotPassword;
     private AlertDialog dialogResetPassword;
 
+    private UserViewModel userViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         setToolBar();
+        setBtnRegister();
+        setBtnEnter();
+    }
+
+    private void setBtnRegister() {
+        btnRegister = findViewById(R.id.btn_login_new_user);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserLoginActivity.this,
+                        UserRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setBtnEnter() {
+        txtEmail = findViewById(R.id.txt_edt_email);
+        txtPassword = findViewById(R.id.txt_edt_password);
+        btnEnter = findViewById(R.id.btn_login_user);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userViewModel.login(txtEmail.getText().toString(),
+                        txtPassword.getText().toString())
+                        .observe(UserLoginActivity.this, new Observer<User>() {
+                            @Override
+                            public void onChanged(User user) {
+                                if(user == null){
+                                    Toast.makeText(getApplicationContext(),
+                                            getString(R.string.msg_login),
+                                            Toast.LENGTH_SHORT).show();
+                                }else{
+                                    finish();
+                                    startActivity(new Intent(UserLoginActivity.this, MainActivity.class));
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     private void setToolBar() {
